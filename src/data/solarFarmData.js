dace9faa86d261a7fc9e665d563a97930c6b50db
yneeraj18 <neerajyellappareddy@gmail.com>
2026-08-27 11:04:05 +0530

    commit

--- src/data/solarFarmData.js
+++ src/data/solarFarmData.js
@@ -1,7 +1,7 @@
 // Smart Solar Farm Initial Data & Simulation Defaults
 
 export const PLANT_INFO = {
-  name: "neeraj sloar farm",
+  name: "vamshi solar farm",
   location: "Bhadla Sun Basin, Rajasthan, IN (27.53° N, 71.91° E)",
   capacityMW: 150,
   installedPanels: 420000,
